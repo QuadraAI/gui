565ad766cfdf450318b2b3dcd5d493ef2b24a8be
MidKnightXI <romain.bouguet@epitech.eu>
2024-03-27 18:33:21 +0100

    feat: adding password type

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import SignIn from "./components/SignIn.vue";
+</script>
+
+<template>
+  <div class="container login-background">
+    <SignIn />
+  </div>
+</template>
+
+<style>
+.login-background {
+  background-image: url('https://source.unsplash.com/random/1920x1080/?landscape');
+  background-size: cover;
+}
+</style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -9,7 +9,7 @@ const password = ref("");
   <form class="login-form" @submit.prevent="greet">
     <h2>Welcome back 👋</h2>
     <input class="signin-input" v-model="username" placeholder="Username" />
-    <input class="signin-input" v-model="password" placeholder="Password" />
+    <input class="signin-input" type="password" v-model="password" placeholder="Password" />
     <button class="submit-button" type="submit">Sign in</button>
   </form>
 </template>
