6c58cf03b7ac67b8ea915b3cc8f96e170ed9c70d
MidKnightXI <romain.bouguet@epitech.eu>
2024-03-27 18:15:28 +0100

    feat: adding background

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import SignIn from "./components/SignIn.vue";
 </script>
 
 <template>
   <div class="container">
-    <Greet />
+    <SignIn />
   </div>
 </template>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+</script>
+
+<template>
+  <form class="login-form" @submit.prevent="greet">
+    <h2>Welcome back 👋</h2>
+    <input class="signin-input" v-model="username" placeholder="Username" />
+    <input class="signin-input" v-model="password" placeholder="Password" />
+    <button class="submit-button" type="submit">Sign in</button>
+  </form>
+</template>
