e04d38d605d34323a2c7204751b7366baa328ccc
MidKnightXI <romain.bouguet@epitech.eu>
2024-09-13 19:05:37 +0200

    feat: now uses the quadra logo in the login page + added title

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,13 +4,25 @@ import SignInCard from "../components/SignInCard.vue";
 
 <template>
     <div class="container login-background">
+        <img src="/quadra-logo-small-backgroundless.webp" alt="Quadra logo backgroundless"/>
+        <h1>Sign in to Quadra</h1>
         <SignInCard />
     </div>
 </template>
 
-<style>
+<style scoped>
 .login-background {
-    background-image: url('https://source.unsplash.com/random/1920x1080/?landscape');
+    background-color: #f5f3ff;
     background-size: cover;
+}
+
+h1 {
+    font-size: 24px;
+    font-weight: 400;
+}
+
+img {
+  max-width: 100px;
+  margin-top: 10px;
 }
 </style>
