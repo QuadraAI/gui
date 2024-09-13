95341b635bb6b1132b2521bef08f77063f8ce062
MidKnightXI <romain.bouguet@epitech.eu>
2024-09-13 14:58:23 +0200

    feat: adding pinia to main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia';
 
 import "./styles.css";
 import App from "./App.vue";
+
 import router from './router';
 
-import { initConfig, setConfigItems } from "./services/config.js"
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app')
 
-initConfig();
-setConfigItems();
-
-createApp(App).use(router).mount('#app')
