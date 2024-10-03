b5c17ed71f58fc11ec062af92b27041574212d1e
MidKnightXI <romain.bouguet@epitech.eu>
2024-10-03 11:52:27 +0200

    feat: now check if the user is authenticated on every routes and check for setup too

--- src/router.js
+++ src/router.js
@@ -2,30 +2,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Setup from './views/Setup.vue'
 import { useAuthStore } from './stores/authStore.js';
-
-const authStore = useAuthStore();
+import { useConfigStore } from './stores/configStore.js';
 
 const routes = [
-    {
-        path: '/home',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/',
-        redirect: () => authStore.isAuthenticated ?   { name: 'Home' } : { name: 'Login' }
-    }
+    { path: '/', name: 'Home', component: Home },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/setup', name: 'Setup', component: Setup },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+    const configStore = useConfigStore();
+    if (configStore.firstLaunch == true && to.name !== "Setup") {
+        next({ name: 'Setup' });
+    }
+    if (!authStore.isAuthenticated && to.name !== 'Login' && to.name !== 'Setup') {
+        return next({ name: 'Login' });
+    }
+    next();
+});
+
 
 export default router
