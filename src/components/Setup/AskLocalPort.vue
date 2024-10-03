f48d06e434bedf773c15fa7da2e0299b79a9a827
MidKnightXI <romain.bouguet@epitech.eu>
2024-10-03 18:16:33 +0200

    feat: adding component to ask local port when the ping failed

---
+++ src/components/Setup/AskLocalPort.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { pingLocalServer } from '../../services/auth';
+import { ref } from 'vue';
+
+if (await pingLocalServer() === true)
+{
+    const router = useRouter();
+    const pushToLogin = () => router.push("/login")
+    pushToLogin()
+}
+
+const port = ref()
+</script>
+
+<template>
+    <input type="text" id="port" v-model.lazy="port" placeholder="5000" />
+    <label for="port">Insert port used</label>
+    <button>Confirm</button>
+</template>
+
+<style scoped>
+</style>
