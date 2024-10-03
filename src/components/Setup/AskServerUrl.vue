b677ab64910ad8290ecfa245293f5cd1ca42a86f
MidKnightXI <romain.bouguet@epitech.eu>
2024-10-03 18:16:49 +0200

    feat: adding component to request remote Core URL

---
+++ src/components/Setup/AskServerUrl.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const url = ref()
+</script>
+
+<template>
+    <input type="text" id="port" v-model.lazy="port" placeholder="5000" />
+    <label for="port">Insert port used</label>
+    <button>Confirm</button>
+</template>
+
+<style scoped>
+</style>
