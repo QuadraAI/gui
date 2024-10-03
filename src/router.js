a207b0e3b639831c0b17cb85d161c38ad5c2f147
MidKnightXI <romain.bouguet@epitech.eu>
2024-10-03 11:57:20 +0200

    feat: adding todo for setup

--- src/router.js
+++ src/router.js
@@ -31,5 +31,4 @@ router.beforeEach((to, from, next) => {
     next();
 });
 
-
 export default router
--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -3,4 +3,9 @@
 
 <template>
     <div>setup</div>
+    <!-- todo: ask if Core is installed on the machine or on a remote server -->
+    <!-- todo: if in remote ask for the IP/URL of the server either ask for the port -->
 </template>
+
+<style scoped>
+</style>
