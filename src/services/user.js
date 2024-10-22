2dcfce74ce4041374cdf7308dad8a9fd57fe90b8
MidKnightXI <romain.bouguet@epitech.eu>
2024-10-22 15:25:21 +0200

    feat: adding user.js service for future interactions with user data

---
+++ src/services/user.js
@@ -0,0 +1,43 @@
+/**
+ * @module user
+ * Handles everything related to the user.
+*/
+
+import { useAuthStore } from '../stores/authStore.js';
+import { useConfigStore } from '../stores/configStore.js';
+
+/**
+ * Change the password of the current user.
+ * @param {string} oldPass The old password for the user.
+ * @param {string} newPass The new password for the user.
+ * @returns {boolean} `true` if the password has been changed.
+ */
+async function ChangePassword(oldPass, newPass) {
+    const configStore = useConfigStore();
+    const authStore = useAuthStore();
+    const url = `${configStore.coreUrl ?? configStore.DEFAULT_URL}/api/user/password`;
+
+    const myHeaders = new Headers({
+        "Content-Type": "application/json",
+        "Authorization": authStore.token
+    });
+
+    const body = JSON.stringify({
+        "password": oldPass,
+        "newPassword": newPass
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: body,
+        redirect: "follow"
+    };
+
+    return await fetch(url, requestOptions)
+        .then(async response => response.ok);
+}
+
+export {
+    ChangePassword,
+};
