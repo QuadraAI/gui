ac425513fb0ca08eeb0b33880a9fc646fdb7e865
MidKnightXI <romain.bouguet@epitech.eu>
2024-11-27 16:40:12 +0100

    feat: adding setup service and hasOnlyDefaultUser method

---
+++ src/services/setup.js
@@ -0,0 +1,15 @@
+/**
+ * @module setup
+ * Module containing methods for the setup of Quadra gui.
+*/
+
+/**
+ * Ask the backend if there is only default user
+ * @returns {bool} - true if there is only default user either false.
+ */
+async function hasOnlyDefaultUser()
+{}
+
+export {
+    hasOnlyDefaultUser
+}
--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -5,23 +5,23 @@ import AskLocalPort from "../components/Setup/AskLocalPort.vue";
 import AskServerUrl from "../components/Setup/AskServerUrl.vue";
 import ChangeAdminInfos from '../components/Setup/ChangeAdminInfos.vue';
 
+import { hasOnlyDefaultUser } from '../services/setup';
+
 /** Define if Core is running locally or not */
 const isLocal = ref(undefined);
 
 /** Define if the current user is the first to connect to Core instance */
-const isFirstUser = true;
+const isFirstUser = await hasOnlyDefaultUser();
 </script>
 
 <template>
-    <!-- todo: ask if Core is installed on the machine or on a remote server -->
-    <!-- todo: if in remote ask for the IP/URL of the server either ask for the port -->
     <div class="container login-background">
         <img src="/quadra-logo-small-backgroundless.webp" alt="Quadra logo backgroundless" />
         <h1>First time here ?</h1>
         <WhereIsCore v-if="isLocal === undefined" v-model:isLocal="isLocal" />
         <AskLocalPort v-else-if="isLocal === true" />
         <AskServerUrl v-else-if="isLocal === false" />
-        <ChangeAdminInfos v-else-if="isFirstUser === false" />
+        <ChangeAdminInfos v-else-if="isFirstUser === true" />
     </div>
 </template>
 
