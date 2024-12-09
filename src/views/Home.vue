b238ecea8b38e022ca6542bc7127723718b9d0a1
MidKnightXI <romain.bouguet@epitech.eu>
2024-12-09 12:32:12 +0100

    feat: adding skeleton for the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+</script>
 <template>
-    <h1>Home</h1>
+    <!-- navbar -->
+    <div>
+        <div>
+            <button>Gallery</button>
+            <button>Modules</button>
+            <button>Rules</button>
+        </div>
+        <!-- todo: add redirect to profile settings/global settings -->
+        <img />
+    </div>
+    <!-- images -->
+    <!-- modules  -->
+    <!-- rules -->
+    <!-- profile settings -->
 </template>
+<style scoped>
+.background {
+    background-color: #f5f3ff;
+    background-size: cover;
+}
+</style>
