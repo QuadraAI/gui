b198f9ca287fc674cc62b964c510fe0df85f97d3
MidKnightXI <romain.bouguet@epitech.eu>
2024-12-24 18:25:57 +0100

    feat: styling WhereIsCore and moving to buttons instead of radios

--- src/components/Setup/WhereIsCore.vue
+++ src/components/Setup/WhereIsCore.vue
@@ -14,27 +14,56 @@ const selectedValue = ref(props.isLocal !== undefined ? String(props.isLocal) : 
 
 const isSelectionValid = computed(() => selectedValue.value !== '');
 
-function confirmSelection() {
+function confirmSelection(value) {
+    selectedValue.value = String(value);
     if (isSelectionValid.value) {
-        emit('update:isLocal', selectedValue.value === 'true');
+        emit('update:isLocal', value);
     }
 }
 </script>
 
 <template>
-    <fieldset>
-        <legend>Is Quadra Core running on this machine?</legend>
-
-        <input type="radio" id="localhost" name="device" value="true" v-model="selectedValue" />
-        <label for="localhost">Yes</label>
-
-        <input type="radio" id="server" name="device" value="false" v-model="selectedValue" />
-        <label for="server">No</label>
-
-        <button @click="confirmSelection" :disabled="!isSelectionValid">
-            Confirm
+    <h1>Is Core running locally ?</h1>
+    <div class="button-group">
+        <button @click="() => confirmSelection(true)" :class="{ active: selectedValue === 'true' }">
+            Yes
         </button>
-    </fieldset>
+        <button @click="() => confirmSelection(false)" :class="{ active: selectedValue === 'false' }">
+            No
+        </button>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 10px;
+    width: 100%;
+    max-width: 200px;
+}
+
+button {
+    margin-top: 7px;
+    padding: 5px 10px;
+    background-color: #3f3f46;
+    color: white;
+    width: 100%;
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+}
+
+button:hover {
+    background-color: #c084fc;
+}
+
+button:active {
+    background-color: #9333ea;
+}
+
+button.active {
+    background-color: #a855f7;
+}
+</style>
